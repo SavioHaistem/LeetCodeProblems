9c39c9f2a348de2bdefbc20acaacf7a65f1fad7f
SavioHaistem <savio.dantas.5858@gmail.com>
2024-04-09 18:34:22 -0300

    remove Kata solutions and not complete solutions

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,6 @@
 import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        //Find the odd int
-        int[] numbers = {20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5}; //5
-        int[] numbersOne = {10}; //5
-        System.out.println(KataOddNumbers.oddNumber(numbers));
+
     }
 }
