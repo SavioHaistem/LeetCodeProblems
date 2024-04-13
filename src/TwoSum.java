e306c5404baf0edfe72bf3b928f9cd05ece5f2ac
SavioHaistem <savio.dantas.5858@gmail.com>
2024-04-12 22:56:32 -0300

    Two sum completed

---
+++ src/TwoSum.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class TwoSum {
+    public static void main(String[] args) {
+        int[] nums = {3,3};
+        int target = 6;
+
+        System.out.println(getTwoSum(nums,target));
+    }
+    public static int[] getTwoSum(int[] nums, int target) {
+        ArrayList<Integer> response = new ArrayList<>(2);
+        for (int number = 0; number < nums.length; number++) {
+            for (int compare = 1; compare < nums.length; compare++) {
+                if (nums[number] + nums[compare] == target && compare != number) {
+                    response.add(number);
+                    response.add(compare);
+                }
+            }
+        }
+
+        //return response.stream().mapToInt(Integer::intValue).toArray();
+        System.out.println(response);
+        return nums;
+    }
+}
